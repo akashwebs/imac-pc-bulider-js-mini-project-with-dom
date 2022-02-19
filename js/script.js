0699668112fd50d40eb7cab64c80421862e62d03
akashwebs <aksah.shil227@gmail.com>
2022-02-19 19:57:18 +0600

    es6 implementation

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const getId = idname => document.getElementById(idname);
+const getTextValue = idName => {
+    return parseFloat(getId(idName).innerText);
+}
+
+
+
+const onClick = (btnId, showId, price) => getId(btnId).addEventListener('click', () => {
+    getId(showId).innerText = price;
+    const bestPrice = getTextValue('best-price') + getTextValue('memory-cost') + getTextValue('storage-cost') + getTextValue('delivery-cost');
+    getId('total-price').innerText = bestPrice;
+})
+
+onClick('eightGB', 'memory-cost', 450)
+onClick('sixteenGB', 'memory-cost', 1250)
+onClick('ssd1', 'storage-cost', 3350)
+onClick('ssd2', 'storage-cost', 4500)
+onClick('ssd3', 'storage-cost', 5050)
+onClick('free-delivery', 'delivery-cost', 0)
+onClick('paid-delivery', 'delivery-cost', 120)
+
+getId('apply-btn').addEventListener('click', () => {
+    const promo = getId('promo-input').value;
+    const offerPromo = "fake";
+    if (promo == offerPromo) {
+        const percent = (getTextValue('total-price') - (getTextValue('total-price') * .2)).toFixed(2);
+
+        getId('total-price').innerText = percent
+    }
+})
